77f8c8f2b9b6d9f96d870059108dd7c32b67a08e
Andrew <kolbe1129@gmail.com>
2021-09-29 15:30:59 -0500

    completed interfaces

--- main.go
+++ main.go
@@ -1,8 +1,55 @@
 package main
 
-import "log"
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	log.Println(numbers[2:])
+	dog := Dog {
+		Name:  "Odin",
+		Breed: "Mutt",
+	}
+	PrintInfo(&dog)
+
+	gorilla := Gorilla {
+		Name: "Bob",
+		Color: "gray",
+		NumberOfTeeth: 38,
+	}
+
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "I like poop"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (g *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (g *Gorilla) NumberOfLegs() int {
+	return 2
 }
