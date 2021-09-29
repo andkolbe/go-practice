b7363e11c7c3f5ab250da6427727e03048de5c34
Andrew <kolbe1129@gmail.com>
2021-09-29 14:23:18 -0500

    completed variables and functions

--- main.go
+++ main.go
@@ -6,8 +6,20 @@ func main() {
 	fmt.Println("Hello, world.")
 
 	var whatToSay string 
+	var i int
 
 	whatToSay = "Goodbye, cruel world"
-
 	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+
+	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
 }
