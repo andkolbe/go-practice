640478376f8878c03fd337a9e1471d2e5f3328cc
Andrew <kolbe1129@gmail.com>
2021-09-29 14:48:04 -0500

    completed types and structs

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
 
 func main() {
-	var myString string
-	myString = "Green"
+	user := User {
+		FirstName: "Andrew",
+		LastName: "Kolbe",
+	}
 
-	log.Println("myString is set to", myString)
-	changeUsingPointer(&myString)
-	log.Println("after func call myString is set to", myString)
-
+	log.Println(user.FirstName, user.LastName)
 }
-
-func changeUsingPointer(s *string) {
-	newValue := "Red"
-	*s = newValue
-}
-
-// a pointer points to a specific location in memory and gives you a means of getting that location in memory
-// when you create a variable, you are creating a variable in memory
-// you store a value at a particular memory location
