d29d5b16fabe1179ffc637ce1a2e2525d1992c60
Andrew <kolbe1129@gmail.com>
2021-09-29 15:40:51 -0500

    completed packages

--- main.go
+++ main.go
@@ -1,55 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+	"github.com/andkolbe/go-practice/helpers"
+)
 
 func main() {
-	dog := Dog {
-		Name:  "Odin",
-		Breed: "Mutt",
-	}
-	PrintInfo(&dog)
-
-	gorilla := Gorilla {
-		Name: "Bob",
-		Color: "gray",
-		NumberOfTeeth: 38,
-	}
-
-	PrintInfo(&gorilla)
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
-}
-
-func (d *Dog) Says() string {
-	return "I like poop"
-}
-
-func (d *Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (g *Gorilla) Says() string {
-	return "Ugh"
-}
-
-func (g *Gorilla) NumberOfLegs() int {
-	return 2
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	fmt.Println(myVar.TypeName)
 }
