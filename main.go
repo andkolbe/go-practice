55c06139777f5730ec505408bab4ee19e12d51d2
Andrew <kolbe1129@gmail.com>
2021-10-18 15:29:19 -0500

    interfaces

--- main.go
+++ main.go
@@ -1,11 +1,56 @@
 package main
 
-import "log"
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-	var mySlice []string
+	dog := Dog {
+		Name:  "Odin",
+		Breed: "Mutt",
+	}
 
-	mySlice = append(mySlice, "Fish")
+	PrintInfo(&dog)
 
-	log.Println(mySlice)
+	gorilla := Gorilla {
+		Name: "Jock",
+		Color: "grey",
+		NumberOfTeeth: 38,
+	}
+
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (D *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (D *Gorilla) NumberOfLegs() int {
+	return 2
 }
