b686de4e9cbe77084b806b9a918409ffc2807e51
Andrew <kolbe1129@gmail.com>
2021-10-18 15:15:38 -0500

    maps

--- main.go
+++ main.go
@@ -2,22 +2,10 @@ package main
 
 import "log"
 
-type myStruct struct {
-	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
-
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "Andrew"
+	var mySlice []string
 
-	myVar2 := myStruct{
-		FirstName: "Winnie",
-	}
+	mySlice = append(mySlice, "Fish")
 
-	log.Println("myVar is set to", myVar.printFirstName())
-	log.Println("myVar2 is set to", myVar2.printFirstName())
+	log.Println(mySlice)
 }
