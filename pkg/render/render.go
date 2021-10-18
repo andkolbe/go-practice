c8461aa6da4dcf6dab96a5f4e9e93813cbdcf37e
Andrew <kolbe1129@gmail.com>
2021-10-18 17:49:13 -0500

    created template cache part 1

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,13 +4,62 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
+// a map of functions that can be used in a template
+var functions = template.FuncMap {
+
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("Error getting template cache:", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+
+	// create a map so we can look up values quickly. The values are the templates that end in .page.tmpl
+	myCache := map[string]*template.Template{}
+
+	// Glob returns the names of all files matching pattern or nil if there is no matching file.
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
