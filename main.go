42235b677438e3db508a4386a60f436273bde886
Andrew <kolbe1129@gmail.com>
2021-10-18 15:52:36 -0500

    made a hello world app

--- main.go
+++ main.go
@@ -1,56 +1,18 @@
 package main
 
-import "fmt"
-
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	dog := Dog {
-		Name:  "Odin",
-		Breed: "Mutt",
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, World")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Number of bytes written: %d", n)
+	})
 
-	PrintInfo(&dog)
-
-	gorilla := Gorilla {
-		Name: "Jock",
-		Color: "grey",
-		NumberOfTeeth: 38,
-	}
-
-	PrintInfo(&gorilla)
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
-}
-
-func (d *Dog) Says() string {
-	return "Woof"
-}
-
-func (D *Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (d *Gorilla) Says() string {
-	return "Ugh"
-}
-
-func (D *Gorilla) NumberOfLegs() int {
-	return 2
+	_ = http.ListenAndServe(":8080", nil)
 }
